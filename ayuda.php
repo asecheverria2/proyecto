765edd32b932cc2efae0bc8f560b25663024e45e
asecheverria2 <echeverriapas@gmail.com>
2020-08-12 13:20:43 -0500

    ayuda cn tablas

--- ayuda.php
+++ ayuda.php
@@ -9,6 +9,13 @@
     <h1>AYUDA QUIERO ESCAPAR DE LATINOAMERICA</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa eius aperiam voluptates repellendus laudantium quam porro doloremque aliquid officiis ea repellat amet, excepturi assumenda eum quae non quibusdam suscipit vel.</p>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum excepturi modi nisi rerum. Iusto eligendi labore autem, a voluptatum, aliquid corrupti, ab cumque adipisci placeat asperiores? Dolore totam voluptate magnam.</p>
-    
+    <table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
 </body>
 </html>
